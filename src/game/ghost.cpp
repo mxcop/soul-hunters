52ed6c25031781e2544497e505dcb0fd84581550
MΛX <mxcop.dev@gmail.com>
2023-07-27 17:06:29 +0200

    feat: deactivate on ghosts

--- src/game/ghost.cpp
+++ src/game/ghost.cpp
@@ -2,27 +2,56 @@
 
 std::vector<Ghost> Ghost::ghosts;
 
-Ghost& Ghost::make(glm::vec2 pos, float hp)
+bool Ghost::make(glm::vec2 pos, float hp)
 {
-	assert(ghosts.size() < ghosts.capacity());
+	auto itr = std::find_if(
+		ghosts.begin(), ghosts.end(),
+		[](Ghost& ghost) {
+			return ghost.is_active == false;
+		}
+	);
+	if (itr != ghosts.end()) {
+		/* Might not be the most efficient to override the memory like this... */
+		*itr = Ghost(pos, hp, itr->id);
 
-	ghosts.push_back(Ghost(pos, hp, ghosts.size()));
+		return true;
+	}
 
-	return ghosts.back();
+	bool has_space = ghosts.size() < ghosts.capacity();
+
+	if (has_space) {
+		ghosts.push_back(Ghost(pos, hp, ghosts.size()));
+	}
+	return has_space;
 }
 
 void Ghost::update_all(glm::vec2 player1, glm::vec2 player2, float dt)
 {
 	for (Ghost& ghost : ghosts) {
-		ghost.update(player1, player2, dt);
+		if (ghost.is_active) ghost.update(player1, player2, dt);
 	}
 }
 
 void Ghost::draw_all(SpriteRenderer& renderer)
 {
 	for (Ghost& ghost : ghosts) {
-		ghost.draw(renderer);
+		if (ghost.is_active) ghost.draw(renderer);
 	}
+}
+
+bool Ghost::deactivate(int id)
+{
+	auto itr = std::find_if(
+		ghosts.begin(), ghosts.end(),
+		[&](Ghost& ghost) {
+			return ghost.id == id;
+		}
+	);
+	if (itr != ghosts.end()) {
+		itr->is_active = false;
+		return true;
+	}
+	return false;
 }
 
 Ghost::Ghost(glm::vec2 pos, float hp, int id)
@@ -61,6 +90,15 @@ void Ghost::update(glm::vec2 player1, glm::vec2 player2, float dt)
 	float dist1 = dist(this->pos, player1);
 	float dist2 = dist(this->pos, player2);
 
+	if (dist1 < 0.1f || dist2 < 0.1f) {
+		deactivate(id);
+
+		Ghost::make({ rand() % 80, rand() % 40 }, 5);
+		Ghost::make({ rand() % 80, rand() % 40 }, 5);
+
+		return;
+	}
+
 	glm::vec2 dir = from_to(dist1 < dist2 ? player1 : player2, this->pos);
 	glm::vec2 side = move_right ? glm::vec2(-dir.y, dir.x) : glm::vec2(dir.y, -dir.x);
 
@@ -68,7 +106,7 @@ void Ghost::update(glm::vec2 player1, glm::vec2 player2, float dt)
 	this->pos += side * (this->speed * (1.0f + std::sin(time * 5.0f) * 4.0f)) * dt;
 }
 
-void Ghost::draw(SpriteRenderer& renderer)
+void Ghost::draw(SpriteRenderer& renderer) const
 {
 	renderer.draw_sprite(ResourceManager::get_texture("ghost"), this->pos, { 2.0f, 2.0f });
 }
--- src/game/ghost.h
+++ src/game/ghost.h
@@ -11,9 +11,11 @@ class Ghost {
 
 public:
     static void reserve_ghosts(size_t num) { ghosts.reserve(num); };
-	static Ghost& make(glm::vec2 pos, float hp);
+	static bool make(glm::vec2 pos, float hp);
     static void update_all(glm::vec2 player1, glm::vec2 player2, float dt);
     static void draw_all(SpriteRenderer& renderer);
+    static std::vector<Ghost>& get_ghosts() { return ghosts; };
+    static bool deactivate(int id);
 
 private:
     Ghost(glm::vec2 pos, float hp, int id);
@@ -29,5 +31,5 @@ private:
     bool move_right;
 
     void update(glm::vec2 player1, glm::vec2 player2, float dt);
-    void draw(SpriteRenderer& renderer);
+    void draw(SpriteRenderer& renderer) const;
 };
